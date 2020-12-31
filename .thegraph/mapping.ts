4141ce7f83ade1c15d042a456821bd3bf65a2a77
Deimantas Špučys <as.deimis@gmail.com>
2020-12-31 12:59:14 +0200

    graph map fix, sablier stream return fix, lender double claim fix

--- .thegraph/mapping.ts
+++ .thegraph/mapping.ts
@@ -1,72 +1,70 @@
-import { store, log } from '@graphprotocol/graph-ts';
-import { ERC721ForLendUpdated, ERC721ForLendUpdatedV2, ERC721ForLendRemoved, ERC721ForLendRemovedV2, LEND721 } from './generated/LEND721/LEND721';
-import { ERC721ForLend, ERC721ForLendV2 } from './generated/schema';
+import { store, log, ethereum } from '@graphprotocol/graph-ts';
 
-export function handleERC721ForLendUpdatedV2(event: ERC721ForLendUpdatedV2): void {
-  const entryId = event.params.tokenAddress.toHex() + '-' + event.params.tokenId.toHex() + '-' + event.params.lenderAddress.toHex();
-  let lendEntry = ERC721ForLendV2.load(entryId);
-  if (lendEntry == null) {
-    lendEntry = new ERC721ForLendV2(entryId);
-  }
-
-  let contract = LEND721.bind(event.address);
-  let contractEntry = contract.try_lendingPool(event.params.tokenAddress, event.params.tokenId, event.params.lenderAddress);
-
-  if (!contractEntry.reverted) {
-    let contractEntryValue = contractEntry.value;
-    lendEntry.durationHours = contractEntryValue.value0;
-    lendEntry.initialWorth = contractEntryValue.value1;
-    lendEntry.earningGoal = contractEntryValue.value2;
-    lendEntry.borrowedAtTimestamp = contractEntryValue.value3;
-    lendEntry.lender = contractEntryValue.value4;
-    lendEntry.borrower = contractEntryValue.value5;
-    lendEntry.lenderClaimedCollateral = contractEntryValue.value6;
-    lendEntry.sablierStreamId = contractEntryValue.value7;
-    lendEntry.tokenAddress = event.params.tokenAddress;
-    lendEntry.tokenId = event.params.tokenId;
-    lendEntry.save()
-  } else {
-    log.warning('ERC721ForLendV2 mapping received reverted', [event.transaction.hash.toHex()])
-  }
-}
+import {
+  ERC721ForLendUpdated,
+  ERC721ForLendRemoved,
+  LEND721,
+  LEND721__lentERC721ListResult
+} from './generated/LEND721/LEND721';
+import { ERC721ForLend } from './generated/schema';
+import {LegacyLEND721} from "./generated/LEND721/LegacyLEND721";
 
 export function handleERC721ForLendUpdated(event: ERC721ForLendUpdated): void {
-  const entryId = event.params.tokenAddress.toHex() + '-' + event.params.tokenId.toHex();
+  let tokenAddress = event.params.tokenAddress;
+  let tokenId = event.params.tokenId;
+
+  let entryId = tokenAddress.toHex() + '-' + tokenId.toHex();
   let lendEntry = ERC721ForLend.load(entryId);
   if (lendEntry == null) {
     lendEntry = new ERC721ForLend(entryId);
   }
 
   let contract = LEND721.bind(event.address);
-  let contractEntry = contract.try_lentERC721List(event.params.tokenAddress, event.params.tokenId);
 
-  if (!contractEntry.reverted) {
-    let contractEntryValue = contractEntry.value;
-    lendEntry.durationHours = contractEntryValue.value0;
-    lendEntry.initialWorth = contractEntryValue.value1;
-    lendEntry.earningGoal = contractEntryValue.value2;
-    lendEntry.borrowedAtTimestamp = contractEntryValue.value3;
-    lendEntry.lender = contractEntryValue.value4;
-    lendEntry.borrower = contractEntryValue.value5;
-    lendEntry.lenderClaimedCollateral = contractEntryValue.value6;
-    lendEntry.sablierStreamId = contractEntryValue.value7;
+  // try latest first
+  let lendEntryResult = contract.try_lentERC721List(tokenAddress, tokenId);
+
+  // if reverted try with legacy contract call without Sablier integration
+  if (lendEntryResult.reverted) {
+    let legacyContract = LegacyLEND721.bind(event.address);
+    lendEntryResult = legacyContract.try_lentERC721List(tokenAddress, tokenId) as ethereum.CallResult<LEND721__lentERC721ListResult>;
+  }
+
+  if (!lendEntryResult.reverted) {
+    let lentEntryValue = lendEntryResult.value;
+
+    lendEntry.durationHours = lentEntryValue.value0;
+    lendEntry.initialWorth = lentEntryValue.value1;
+    lendEntry.earningGoal = lentEntryValue.value2;
+    lendEntry.borrowedAtTimestamp = lentEntryValue.value3;
+    lendEntry.lender = lentEntryValue.value4;
+    lendEntry.borrower = lentEntryValue.value5;
+    lendEntry.lenderClaimedCollateral = lentEntryValue.value6;
     lendEntry.tokenAddress = event.params.tokenAddress;
     lendEntry.tokenId = event.params.tokenId;
+
+    // sablierStreamId is optional
+    if (!lentEntryValue.value7 || lentEntryValue.value7.isZero()) {
+      lendEntry.sablierStreamId = null;
+    } else {
+      lendEntry.sablierStreamId = lentEntryValue.value7;
+    }
+
     lendEntry.save()
   } else {
-    log.warning('ERC721ForLend received reverted', [event.transaction.hash.toHex()])
+    log.warning(
+        'ERC721ForLend received reverted for ' + entryId +
+        ' on transaction hash ' + event.transaction.hash.toHex() +
+        ' for LEND721 contract ' + event.address.toHex() +
+        ' on NFT contract ' + tokenAddress.toHex() + '-' + tokenId.toHex()
+    , [
+      event.block.number.toHex()
+    ])
   }
 }
 
-export function handleERC721ForLendRemovedV2(event: ERC721ForLendRemovedV2): void {
-  const entryId = event.params.tokenAddress.toHex() + '-' + event.params.tokenId.toHex() + '-' + event.params.lenderAddress.toHex();
-  let lendEntry = ERC721ForLendV2.load(entryId);
-  if (lendEntry == null) return;
-  store.remove('ERC721ForLendV2', entryId);
-}
-
 export function handleERC721ForLendRemoved(event: ERC721ForLendRemoved): void {
-  const entryId = event.params.tokenAddress.toHex() + '-' + event.params.tokenId.toHex();
+  let entryId = event.params.tokenAddress.toHex() + '-' + event.params.tokenId.toHex();
   let lendEntry = ERC721ForLend.load(entryId);
   if (lendEntry == null) return;
   store.remove('ERC721ForLend', entryId);
